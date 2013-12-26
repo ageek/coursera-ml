c6379ae142737c6c8e8b096c05f7603d708a64bb
ageek <re.90@live.com>
2013-12-26 08:21:05 +0530

    Exercise 7 solutions

---
+++ mlclass-ex7-004/computeCentroids.m
@@ -0,0 +1,51 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returs the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+
+for i=1:K;
+	% get binary array with 1 for all those elements 
+	% whose centroid is i
+	id = (idx == i);
+	howMany = sum(id);
+	%sum of all elements belonging to this centroid
+	% size of id: m x 1
+	% size of X:  m x n
+	sumVec =  id' * X;
+	size(centroids(i, :));
+	centroids(i,:) = sumVec ./ howMany ;
+end;
+
+
+
+
+
+
+% =============================================================
+
+
+end
+
